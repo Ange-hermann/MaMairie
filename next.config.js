ba6c238f4ad6833640ab1ac32599cdf9b55ee1d4
AngeHermann24 <angeherboua@gmail.com>
2026-03-25 02:22:11 +0000

    🔧 Fix Netlify Config - Optimiser configuration pour Next.js et fichiers statiques

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['localhost'],
+    unoptimized: true,
+    domains: ['localhost', 'gpjydaygjapdrkgppfid.supabase.co'],
   },
 }
 
